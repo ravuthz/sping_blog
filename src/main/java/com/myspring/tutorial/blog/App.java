06855302828a58283e7f46923d0555e68c8f8b1b
ravuthz <ravuthz@gmail.com>
2016-10-25 23:30:48 +0700

    Replace File System Contexts with Class Path Context

--- src/main/java/com/myspring/tutorial/blog/App.java
+++ src/main/java/com/myspring/tutorial/blog/App.java
@@ -1,7 +1,8 @@
 package com.myspring.tutorial.blog;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+//import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 /**
  * Hello world!
@@ -11,7 +12,9 @@ public class App
 {
     public static void main( String[] args )
     {
-		ApplicationContext context = new FileSystemXmlApplicationContext("src/main/java/com/myspring/tutorial/blog/beans/beans.xml");
+		ApplicationContext context = 
+//			new FileSystemXmlApplicationContext("src/main/java/com/myspring/tutorial/blog/beans/beans.xml");
+			new ClassPathXmlApplicationContext("com/myspring/tutorial/blog/beans/beans.xml");
         
         Person person = (Person) context.getBean("person");
         person.setFirstName("Ravuthz");
@@ -19,6 +22,7 @@ public class App
         String name = person.getFirstName();
         System.out.println("Person firstName: " + name);
         
-        ((FileSystemXmlApplicationContext) context).close();
+//        ((FileSystemXmlApplicationContext) context).close();
+        ((ClassPathXmlApplicationContext) context).close();
     }
 }
