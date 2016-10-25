cada5cb91ff5b7711d1ba14587f7de1e4c698767
ravuthz <ravuthz@gmail.com>
2016-10-25 23:21:26 +0700

    Move basic beans to beans package

--- src/main/java/com/myspring/tutorial/blog/App.java
+++ src/main/java/com/myspring/tutorial/blog/App.java
@@ -11,7 +11,7 @@ public class App
 {
     public static void main( String[] args )
     {
-		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		ApplicationContext context = new FileSystemXmlApplicationContext("src/main/java/com/myspring/tutorial/blog/beans/beans.xml");
         
         Person person = (Person) context.getBean("person");
         person.setFirstName("Ravuthz");
