b625a676cc05f8bb16778841738be830f9eb130b
ravuthz <ravuthz@gmail.com>
2016-10-25 22:04:34 +0700

    Initialize Maven Project

---
+++ src/main/java/com/myspring/tutorial/blog/App.java
@@ -0,0 +1,13 @@
+package com.myspring.tutorial.blog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
